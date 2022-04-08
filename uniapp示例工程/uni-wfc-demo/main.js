d338a6c53bceff1879310c2ccea3888c5600680b
imndx <imndxx@gmail.com>
2022-04-08 22:05:18 +0800

    add store

--- uniapp示例工程/uni-wfc-demo/main.js
+++ uniapp示例工程/uni-wfc-demo/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import store from "./store";
 
 Vue.config.productionTip = false
 
@@ -8,4 +9,7 @@ App.mpType = 'app'
 const app = new Vue({
     ...App
 })
+
+app.store = store;
+
 app.$mount()
